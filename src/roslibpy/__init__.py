31e608ccdf2120460f19165bc6ae09d147872406
Gonzalo Casas <casas@arch.ethz.ch>
2018-02-19 09:51:15 +0100

    Add all versioning and metadata details

--- src/roslibpy/__init__.py
+++ src/roslibpy/__init__.py
@@ -1,3 +1,6 @@
-from .core import Message, Topic, ServiceRequest, ServiceResponse
-from .services import Service
+from .__version__ import (__author__, __author_email__, __copyright__,
+                          __description__, __license__, __title__, __url__,
+                          __version__)
+from .core import Message, ServiceRequest, ServiceResponse, Topic
 from .ros import Ros
+from .services import Service
---
+++ src/roslibpy/__version__.py
@@ -0,0 +1,11 @@
+# 72 6F 73 6C 69 62 70 79
+# 47 72 61 6D 61 7A 69 6F  4B 6F 68 6C 65 72  52 65 73 65 61 72 63 68
+
+__title__ = 'roslibpy'
+__description__ = 'Python ROS Bridge library.'
+__url__ = 'https://github.com/gramaziokohler/roslibpy'
+__version__ = '0.1.0'
+__author__ = 'Gramazio Kohler Research'
+__author_email__ = 'gramaziokohler@arch.ethz.ch'
+__license__ = 'MIT license'
+__copyright__ = 'Copyright 2018 Gramazio Kohler Research'
